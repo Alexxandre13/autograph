f41568d3c452d69d9b2d827b26fe4a1c18ebdb29
Alex <ajm.dosreis.daponte@gmail.com>
2021-10-08 11:57:15 +0200

    Option OK, suppression OK

--- src/Controller/AutoController.php
+++ src/Controller/AutoController.php
@@ -2,25 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\StatusRepository;
 use App\Repository\VehiculeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AutoController extends AbstractController
 {
-    public function __construct(VehiculeRepository $vehiculeRepo)
+    public function __construct(
+        VehiculeRepository $vehiculeRepo,
+        StatusRepository $statusRepo,
+        EntityManagerInterface $em
+        )
     {
         $this->vehiculeRepo = $vehiculeRepo;
+        $this->statusRepo = $statusRepo;
+        $this->em = $em;
     }
 
     /**
-     * @Route("/", name="carRead")
+     * @Route("/", name="carIndex")
      */
     public function index(): Response
     {
         return $this->render('auto/index.html.twig', [
-            'vehicules' => $this->vehiculeRepo->findAll()
+            'vehicules' => $this->vehiculeRepo->findAll(),
+            'statuses' => $this->statusRepo->findAll()
         ]);
     }
 
@@ -29,28 +38,35 @@ class AutoController extends AbstractController
      */
     public function create(): Response
     {
-        return $this->render('auto/index.html.twig', [
-            'vehicules' => $this->vehiculeRepo->findAll()
+        return $this->render('auto/create.html.twig');
+    }
+
+    /**
+     * @Route("/editer/{id<\d+>}", name="carUpdate")
+     */
+    public function update($id): Response
+    {
+        $vehicule = $this->vehiculeRepo->findOneBy(['id' => $id]);
+
+        if (!$vehicule) throw $this->createNotFoundException("Ce véhicule n'existe pas !");
+
+        return $this->render('auto/update.html.twig', [
+            'vehicule' => $vehicule
         ]);
     }
 
     /**
-     * @Route("/editer", name="carUpdate")
+     * @Route("/supprimer/{id<\d+>}", name="carDelete")
      */
-    public function update(): Response
+    public function delete($id): Response
     {
-        return $this->render('auto/index.html.twig', [
-            'vehicules' => $this->vehiculeRepo->findAll()
-        ]);
-    }
+        $vehicule = $this->vehiculeRepo->findOneBy(['id' => $id]);
 
-    /**
-     * @Route("/supprimer", name="carDelete")
-     */
-    public function delete(): Response
-    {
-        return $this->render('auto/index.html.twig', [
-            'vehicules' => $this->vehiculeRepo->findAll()
-        ]);
+        if (!$vehicule) throw $this->createNotFoundException("Ce véhicule n'existe pas !");
+
+        $this->em->remove($vehicule);
+        $this->em->flush();
+
+        return $this->redirectToRoute('carIndex');
     }
 }
